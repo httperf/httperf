f9498483d3495ecbee2fe64ee3c123a9d362b18e
tbbulloc <tbbulloc@2618b828-e6a1-11de-b136-e12340cf38ae>
2007-03-31 21:53:01 +0000

    * *.c and *.h: Adjusted license to explicitly list HP copyright as Copyright 2000-2007 Hewlett-Packard Company

--- httperf/src/gen/session.c
+++ httperf/src/gen/session.c
@@ -1,7 +1,7 @@
 /*
     httperf -- a tool for measuring web server performance
-    Copyright (C) 2007  Hewlett-Packard Company and Contributors
-    Contributed by David Mosberger-Tang
+    Copyright 2000-2007 Hewlett-Packard Company and Contributors listed in
+    AUTHORS file. Originally contributed by David Mosberger-Tang
 
     This file is part of httperf, a web server performance measurment
     tool.
