96fad03155d45c95f89903f134e4d4f3cba6ee36
tbbulloc <tbbulloc@2618b828-e6a1-11de-b136-e12340cf38ae>
2007-05-27 20:03:23 +0000

    Automatically attempt to update config.guess and config.sub if wget is installed

--- trunk/httperf/autogen.sh
+++ trunk/httperf/autogen.sh
@@ -48,17 +48,12 @@ autoconf
 # from GNU CVS
 WGET=`which wget`
 if [ "x$WGET" != "x" ]; then
-    echo "Autoupdate config.sub and config.guess (y/n)?"
-    read IN
-    if [ "$IN" = "y" ] || [ "$IN" = "Y" ]; then
 	wget -O tmpfile http://savannah.gnu.org/cgi-bin/viewcvs/*checkout*/config/config/config.guess
 	mv tmpfile config.guess
 	wget -O tmpfile http://savannah.gnu.org/cgi-bin/viewcvs/*checkout*/config/config/config.sub
 	mv tmpfile config.sub
-    fi
 else
-    echo "Could not autoupdate config.sub and config.guess"
+    echo "wget not installed - Could not autoupdate config.sub and config.guess"
 fi
 
 echo "Finished!"
-
