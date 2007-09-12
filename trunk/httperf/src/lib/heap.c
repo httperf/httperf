3b11c6f492ec0b5dff25814094a710ae1c3fa8e0
tedbullock <tedbullock@2618b828-e6a1-11de-b136-e12340cf38ae>
2007-09-12 04:32:00 +0000

    lib/heap.c, lib/heap.h: New generic priority queue data structure

---
+++ trunk/httperf/src/lib/heap.c
@@ -0,0 +1,167 @@
+/*
+ * Copyright (C) 2007 Ted Bullock <tbullock@canada.com>
+ * 
+ * This file is part of httperf, a web server performance measurment tool.
+ * 
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ * 
+ * In addition, as a special exception, the copyright holders give permission
+ * to link the code of this work with the OpenSSL project's "OpenSSL" library
+ * (or with modified versions of it that use the same license as the "OpenSSL" 
+ * library), and distribute linked combinations including the two.  You must
+ * obey the GNU General Public License in all respects for all of the code
+ * used other than "OpenSSL".  If you modify this file, you may extend this
+ * exception to your version of the file, but you are not obligated to do so.
+ * If you do not wish to do so, delete this exception statement from your
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA 
+ */
+
+#include "config.h"
+
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include <generic_types.h>
+#include <heap.h>
+
+#define Minimum_Heap_Size 10
+
+struct Heap {
+	u_long          array_size;
+	u_long          num_elements;
+	heap_compare    compare;
+	Any_Type        storage[];	/* c99 Flexible Array Member */
+};
+
+struct Heap    *
+create_heap(u_long size, heap_compare compare_callback)
+{
+	struct Heap    *h;
+
+	if (size < Minimum_Heap_Size)
+		size = Minimum_Heap_Size;
+
+	/*
+	 * We are using c99 Flexible Array Members so we can do this :) 
+	 */
+	h = malloc(sizeof(struct Heap) + sizeof(Any_Type) * size);
+	if (!h)
+		return NULL;
+
+	h->array_size = size;
+	h->num_elements = 0;
+	h->compare = compare_callback;
+
+	return h;
+}
+
+_Bool
+is_heap_empty(struct Heap * h)
+{
+	return h->num_elements == 0;
+}
+
+_Bool
+is_heap_full(struct Heap * h)
+{
+	return h->array_size == h->num_elements;
+}
+
+u_long
+num_heap_elements(struct Heap * h)
+{
+	return h->num_elements;
+}
+
+void
+free_heap(struct Heap *h)
+{
+	if (h != NULL)
+		free(h);
+}
+
+#define PARENT(i)	(i/2)
+_Bool
+insert(Any_Type a, struct Heap *h)
+{
+	u_long          i, parent;
+
+	if (is_heap_full(h))
+		return false;
+
+	i = ++h->num_elements;
+
+	/*
+	 * find the correct place to insert
+	 */
+	while ((i > 1) && (*h->compare) (h->storage[PARENT(i)], a)) {
+		h->storage[i] = h->storage[PARENT(i)];
+		i = PARENT(i);
+	}
+	h->storage[i] = a;
+	return true;
+}
+
+static void
+percolate(struct Heap *h, u_long hole)
+{
+	int             child;
+	Any_Type        dest_val = h->storage[hole];
+
+	for (; hole * 2 <= h->num_elements; hole = child) {
+		child = hole * 2;
+		if (child != h->num_elements
+		    && (*h->compare) (h->storage[child + 1],
+				      h->storage[child + 1]))
+			child++;
+		if ((*h->compare) (h->storage[child + 1], dest_val))
+			h->storage[hole] = h->storage[child];
+		else
+			break;
+	}
+	h->storage[hole] = dest_val;
+}
+
+Any_Type
+remove_min(struct Heap * h)
+{
+	if(is_heap_empty(h))
+		return (Any_Type) 0;
+	else {
+		Any_Type        min = h->storage[1];
+		h->storage[1] = h->storage[h->num_elements--];
+		percolate(h, 1);
+		
+		return min;
+	}
+}
+
+Any_Type
+poll_min(struct Heap * h)
+{
+	if(is_heap_empty(h))
+		return (Any_Type) 0;
+	else
+		return h->storage[1];
+}
+
+void
+heap_for_each(struct Heap *h, heap_for_each_action action)
+{
+	for(u_long i = 1; i <= h->num_elements; i++)
+	{
+		(*action)(h->storage[i]);
+	}
+}
